2f4ec51fb1807df22b44a8d8d2426fc344137514
Alexander-Shiryaev <69469715+Alexander-Shiryaev@users.noreply.github.com>
2020-12-07 11:19:43 +0300

    Add files via upload
    Added vue-router

--- src/components/v-cart.vue
+++ src/components/v-cart.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="v-cart"></div>
+  <div class="v-cart">
+    <router-link :to="{ name: 'catalog' }">
+      <div class="v-catalog__link_to_cart">Back to Catalog</div>
+    </router-link>
+    <h1>Cart</h1>
+    <p v-if="!cart_data.length">There are no products in cart...</p>
+    <v-cart-item
+      v-for="(item, index) in cart_data"
+      :key="item.article"
+      :cart_item_data="item"
+      @deleteFromCart="deleteFromCart(index)"
+    />
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "v-cart",
-        props: {},
-        data() {
-            return {}
-        },
-        computed: {}
-    }
+import vCartItem from "./v-cart-item.vue";
+import { mapActions } from "vuex";
+
+export default {
+  name: "v-cart",
+  components: {
+    vCartItem,
+  },
+  props: {
+    cart_data: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    ...mapActions(["DELETE_FROM_CART"]),
+    deleteFromCart(index) {
+      this.DELETE_FROM_CART(index);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/components/v-main-wrapper.vue
+++ src/components/v-main-wrapper.vue
@@ -1,21 +1,15 @@
 <template>
     <div class="v-main-wrapper">
-        <p>{{title}}</p>
-        <v-catalog />
-        <v-cart />
+        <keep-alive>
+            <router-view></router-view> 
+        </keep-alive>
     </div>
 </template>
 
 <script>
-    import vCatalog from './v-catalog.vue'
-    import vCart from './v-cart.vue'
 
     export default {
         name: "v-main-wrapper",
-        components: {
-            vCatalog,
-            vCart
-        },
         props: {},
         data() {
             return {
@@ -24,19 +18,12 @@
         },
         computed: {},
         methods: {},
-        watch: {},
-        mounted() {
-            console.log('Hello, I am alive!')
-        }
+        watch: {}
     }
 </script>
 
 <style>
     .v-main-wrapper {
-        /* display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center; */
         max-width: 900px;
         margin: 0 auto;
     }
